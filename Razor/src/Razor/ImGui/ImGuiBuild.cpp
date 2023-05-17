e9f96b87de8f04a3793076a13c572c6e7d4a6fde
Mirko Adzic <adzicmirko97@gmail.com>
2023-05-17 23:06:31 +0200

    Docking support for ImGui pt2

---
+++ Razor/src/Razor/ImGui/ImGuiBuild.cpp
@@ -0,0 +1,8 @@
+/* Mire, 5/17/2023
+   This file is used to include certain ImGui dependencies in the project */
+
+#include "rzr_pch.h"
+
+#define IMGUI_IMPL_OPENGL_LOADER_CUSTOM
+#include "backends/imgui_impl_opengl3.cpp"
+#include "backends/imgui_impl_glfw.cpp"
